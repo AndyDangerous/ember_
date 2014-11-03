0bb887940ea1ff8fb9d876f977221c2ee76617d7
Andy Mention <amention@gmail.com>
2014-11-02 22:07:57 -0700

    SHE'S WORKING

--- app/routes/speakers/create.js
+++ app/routes/speakers/create.js
@@ -7,9 +7,11 @@ export default Ember.Route.extend({
     });
   },
   
-  actions: function() {
-    var my_model = this.controller.get('model');
-    my_model.save();
-    this.transitionTo('speakers.show', my_model);
-  },
+  actions: {
+    create: function() {
+      var my_model = this.controller.get('model');
+      my_model.save();
+      this.transitionTo('speakers.show', my_model);
+    }
+  }
 });
