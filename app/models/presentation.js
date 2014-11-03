55d204721aa6aa10d3053cfc56621319e64ec1e8
Andy Mention <amention@gmail.com>
2014-11-02 18:48:11 -0700

    Presentations now belong to speakers asynchronously. I don't know what that is.

--- app/models/presentation.js
+++ app/models/presentation.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  speaker: DS.belongsTo('speaker')
+  speaker: DS.belongsTo('speaker', { async: true })
 });
