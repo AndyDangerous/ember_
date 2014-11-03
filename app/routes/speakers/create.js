d3ab2ce0b05418312267d2155adf4f114fc12dd0
Andy Mention <amention@gmail.com>
2014-11-02 19:00:11 -0700

    Set up routes for CRUD actions

---
+++ app/routes/speakers/create.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('speaker', {
+      name: ''
+    });
+  },
+  
+  actions: function() {
+    var my_model = this.controller.get('model');
+    my_model.save();
+    this.transitionTo('speakers.show', my_model);
+    }
+  }
+});
