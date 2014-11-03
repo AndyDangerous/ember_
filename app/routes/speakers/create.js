ea349189d0968f6d038f3f6507fff0fdf5440658
Andy Mention <amention@gmail.com>
2014-11-02 19:24:05 -0700

    update /templates/speakers/*

--- app/routes/speakers/create.js
+++ app/routes/speakers/create.js
@@ -11,6 +11,5 @@ export default Ember.Route.extend({
     var my_model = this.controller.get('model');
     my_model.save();
     this.transitionTo('speakers.show', my_model);
-    }
-  }
+  },
 });
--- app/routes/speakers/show.js
+++ app/routes/speakers/show.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   
   actions:{
     delete: function() {
-    var my_model = this.controller.get('model').destroyRecord();
+    this.controller.get('model').destroyRecord();
     this.transitionTo('speakers.index');
     }
   }
