1707e41885583df67c7c0a205f771c70f661d54f
Andy Mention <amention@gmail.com>
2014-11-02 19:30:31 -0700

    Reverse that asynchronous stuff

--- app/models/presentation.js
+++ app/models/presentation.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  speaker: DS.belongsTo('speaker', { async: true })
+  speaker: DS.belongsTo('speaker')
 });
